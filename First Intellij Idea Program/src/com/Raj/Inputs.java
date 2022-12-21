d38b050827b0229a196a5448524d336ae12f6860
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-12-21 19:43:51 +0530

    learnt about inputs in java

--- First Intellij Idea Program/src/com/Raj/Inputs.java
+++ First Intellij Idea Program/src/com/Raj/Inputs.java
@@ -1,4 +1,15 @@
 package com.Raj;
 
+import java.util.Scanner;
+
 public class Inputs {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter some input: ");
+        int rollno = input.nextInt();
+        System.out.println("Your roll number is: " + rollno);
+
+        int a = 234_000_000; //underscores will get ignored and a comma will get added
+        System.out.println(a);
+    }
 }
