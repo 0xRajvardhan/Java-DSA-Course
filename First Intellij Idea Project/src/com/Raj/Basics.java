d5e1e17de9cb6bdc0a9e65677af0424017014839
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-12-21 23:14:35 +0530

    while and for loop basics in Java

---
+++ First Intellij Idea Project/src/com/Raj/Basics.java
@@ -0,0 +1,26 @@
+package com.Raj;
+
+public class Basics {
+    public static void main(String[] args) {
+
+        //if statement
+        int a = 10;
+        if (a == 10) {
+            System.out.println("Yea bro");
+        }
+
+        //while loop
+        int count = 1;
+        while (count <= 5) {
+            System.out.println(count);
+            count++;
+        }
+
+        //for loop
+
+        for(int number = 1; number != 5; number++) {
+            System.out.println(number);
+        }
+
+    }
+}
