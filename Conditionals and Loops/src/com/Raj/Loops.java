95f0cc5885ae1642abc5ef5d1c21504b9964bc0a
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-12-22 11:23:25 +0530

    for loop in java

--- Conditionals and Loops/src/com/Raj/Loops.java
+++ Conditionals and Loops/src/com/Raj/Loops.java
@@ -1,4 +1,32 @@
 package com.Raj;
 
+import java.util.Scanner;
+
 public class Loops {
+    public static void main(String[] args) {
+
+        /*
+        Syntax of for loops -
+
+        for(initialisation; condition; increment/decrement) {
+        //body
+        }
+         */
+
+        //Q: Print numbers from 1 to 5
+        for (int num = 1; num <= 5; num ++){
+            System.out.println(num);
+        }
+
+        //Q: Print numbers from 1 to n
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = in.nextInt();
+
+        for (int num = 1; num <= n; num++ ) {
+//            System.out.println(num);
+            System.out.println("I will complete my homework");
+        }
+
+    }
 }
