cb0204bce6c68477447a1b0b0b90f785e8e7b009
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-12-23 23:09:35 +0530

    learned about enhanced switch cases in Java

--- SwtichCases/src/com/Raj/Main.java
+++ SwtichCases/src/com/Raj/Main.java
@@ -6,14 +6,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String fruit = in.next();
-
-        if (fruit.equals("mango")) {
-            System.out.println("King of fruits");
-        }
-
-        if (fruit.equals("apple")) {
-            System.out.println("Apple a day keeps the doctor away");
-
+        // instead of IFs we can use SwitchCases
+        switch (fruit) {
+            case "Mango":
+                System.out.println("King of fruits");
+                break;
+            case "Apple":
+                System.out.println("Apple a day keeps the doctor away");
+                break;
+            case "Orange":
+                System.out.println("Sour af");
+                break;
+            case "Grapes":
+                System.out.println("Vine fruit");
+                break;
+            default:
+                System.out.println("Please enter valid fruit");
         }
     }
 }
