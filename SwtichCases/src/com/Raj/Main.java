00e6477336ac803a50bb63197a245443ebb4d49e
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-12-23 23:22:59 +0530

    switch case another example

--- SwtichCases/src/com/Raj/Main.java
+++ SwtichCases/src/com/Raj/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.print("Enter a fruit name: ");
         String fruit = in.next();
         // instead of IFs we can use SwitchCases
         switch (fruit) {
@@ -13,6 +14,17 @@ public class Main {
             case "Orange" -> System.out.println("Sour af");
             case "Grapes" -> System.out.println("Vine fruit");
             default -> System.out.println("Please enter valid fruit");
+        }
+        System.out.print("Enter a number from 1-7: ");
+        int day = in.nextInt();
+        switch (day) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
         }
     }
 }
