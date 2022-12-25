2969540a0b0e46a94f29209351bf8dab8bf0b156
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-12-25 18:28:35 +0530

    string method

---
+++ Methods_or_functions/src/com/Raj/StringExample.java
@@ -0,0 +1,13 @@
+package com.Raj;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+
+    static String greet() {
+        String greeting = "How are you?";
+        return greeting;
+    }
+}
