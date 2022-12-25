991ad499b789671000aea1512fb85f519ab46f8c
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-12-25 18:19:44 +0530

    learnt about return type and buit a return statement

--- Methods_or_functions/src/com/Raj/Sum.java
+++ Methods_or_functions/src/com/Raj/Sum.java
@@ -5,6 +5,20 @@ import java.util.Scanner;
 public class Sum {
     public static void main(String[] args) {
         sum();
+        int ans = sum2();
+        System.out.println(ans);
+    }
+
+    static int sum2() {
+        Scanner in = new Scanner(System.in);
+        int num1, num2, sum;
+
+        System.out.print("Enter first number: ");
+        num1 = in.nextInt();
+        System.out.print("Enter second number: ");
+        num2 = in.nextInt();
+        sum = num1 + num2;
+        return sum; //this function is over here if you put anything after this return statement it will never get executed.
     }
 
     static void sum() {
@@ -16,7 +30,7 @@ public class Sum {
         System.out.print("Enter second number: ");
         num2 = in.nextInt();
         sum = num1 + num2;
-        System.out.print("The sum is: " + sum);
+        System.out.println("The sum is: " + sum);
     }
 
     /*
