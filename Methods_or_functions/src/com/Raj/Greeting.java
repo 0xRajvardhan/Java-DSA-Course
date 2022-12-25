d688973a8fa97499f3996c9e7c1d84498128737b
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-12-25 18:13:01 +0530

    made a simple method for greeting

---
+++ Methods_or_functions/src/com/Raj/Greeting.java
@@ -0,0 +1,12 @@
+package com.Raj;
+
+public class Greeting {
+    public static void main(String[] args) {
+        greeting();
+    }
+
+    static void greeting() {
+        System.out.println("Hello Bruh");
+    }
+
+}
