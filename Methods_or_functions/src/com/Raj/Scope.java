61c8cf042a28451ff8883d76ad4a06e9be43e17e
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-12-26 22:42:49 +0530

    scope of methods in java

---
+++ Methods_or_functions/src/com/Raj/Scope.java
@@ -0,0 +1,14 @@
+package com.Raj;
+
+public class Scope {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a + " " + b);
+    }
+    static void random(int marks) {
+        int num = 67;
+        System.out.println(num);
+    }
+
+}
