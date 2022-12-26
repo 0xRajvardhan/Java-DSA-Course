13ec1ece7f53c091fb1b15acd8fd9b3cfa60f05b
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-12-26 22:49:18 +0530

    learnt about scope of block in java

--- Methods_or_functions/src/com/Raj/Scope.java
+++ Methods_or_functions/src/com/Raj/Scope.java
@@ -5,7 +5,18 @@ public class Scope {
         int a = 10;
         int b = 20;
         System.out.println(a + " " + b);
+        String name = "Rajvardhan";
+        {
+//            int a = 78; already initialised outside the block in the same method hence you can only change the value not initialise it again
+            a = 78; // re-assigned the value
+            int c = 90; //values initialised in this block will remain in the block only
+            name = "Kunal";
+        }
+        System.out.println(name);
+        System.out.println(a);
+//        System.out.println(c); thi will throw an error because of block scope
     }
+
     static void random(int marks) {
         int num = 67;
         System.out.println(num);
