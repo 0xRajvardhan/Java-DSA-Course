2c8cf4514b3ae271f2aaff97838df0e3fdfb5ec6
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-12-27 23:10:39 +0530

    armstrong number problem using methods in java

--- Methods_or_functions/src/com/Raj/Questions.java
+++ Methods_or_functions/src/com/Raj/Questions.java
@@ -5,11 +5,32 @@ import java.util.Scanner;
 public class Questions {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+//        int n = in.nextInt();
 //        System.out.println(isPrime(n));
-        boolean ans = isPrime(n);
-        System.out.println(ans);
+//        boolean ans = isPrime(n);
+//        System.out.println(ans);
+        for (int i = 100; i < 1000 ; i++) {
+            if (isArmstrong(i)) {
+                System.out.print(i + " ");
+            }
+        }
     }
+
+    // print all the 3 digit armstrong numbers
+    static boolean isArmstrong(int n) {
+        int original = n;
+        int sum = 0;
+
+        while (n > 0) {
+            int rem = n % 10;
+            n = n / 10;
+            sum = sum + rem * rem * rem;
+        }
+
+        return sum == original;
+
+    }
+
 
     static boolean isPrime(int n) {
         if (n <= 1) {
