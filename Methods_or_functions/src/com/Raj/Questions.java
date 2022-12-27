fe9769bd35b7c6312da67d5a359cdb582ae533e3
Rajvardhan <singhrajvardhan2003@gmail.com>
2022-12-27 22:42:03 +0530

    prime number question using methods in java

---
+++ Methods_or_functions/src/com/Raj/Questions.java
@@ -0,0 +1,27 @@
+package com.Raj;
+
+import java.util.Scanner;
+
+public class Questions {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+//        System.out.println(isPrime(n));
+        boolean ans = isPrime(n);
+        System.out.println(ans);
+    }
+
+    static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        int c = 2;
+        while (c * c <= n) {
+            if (n % c == 0) {
+                return false;
+            }
+            c++;
+        }
+        return c * c > n;
+    }
+}
