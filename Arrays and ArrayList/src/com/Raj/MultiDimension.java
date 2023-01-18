180c2151088b8dbd4a18c5b1bfc9e8b4ebef3a9d
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-01-18 23:28:59 +0530

    I/O in 2D arrays in java

--- Arrays and ArrayList/src/com/Raj/MultiDimension.java
+++ Arrays and ArrayList/src/com/Raj/MultiDimension.java
@@ -3,6 +3,7 @@ package com.Raj;
 import java.lang.reflect.Array;
 import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class MultiDimension {
     public static void main(String[] args) {
@@ -11,11 +12,33 @@ public class MultiDimension {
             4 5 6
             7 8 9
          */
+        Scanner in = new Scanner(System.in);
 //        int[][] arr = new int[3][]; // adding rows is mandatory but not rows
-        int[][] arr2D = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
-        }; // this is a direct way
+//        int[][] arr = {
+//                {1, 2, 3},
+//                {4, 5, 6},
+//                {7, 8, 9}
+//        }; // this is a direct way
+
+        int[][] arr = new int[3][3];
+        System.out.println(arr.length); //number of rows
+        //input
+        for (int row = 0; row < arr.length; row++) {
+            // for each column in every row
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col] = in.nextInt();
+            }
+        }
+
+        //output
+//        for (int row = 0; row < arr.length; row++) {
+//            System.out.println(Arrays.toString(arr[row]));
+//            System.out.println();
+//        }
+
+        for (int[] a : arr) {
+            System.out.println(Arrays.toString(a));
+            System.out.println();
+        }
     }
 }
