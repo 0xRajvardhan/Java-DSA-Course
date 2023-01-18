564ddc3b5034dbaaed37fbcd8361cda52b4c6e3e
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-01-18 12:39:00 +0530

    2D Arrays in Java and internal working

---
+++ Arrays and ArrayList/src/com/Raj/MultiDimension.java
@@ -0,0 +1,21 @@
+package com.Raj;
+
+import java.lang.reflect.Array;
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+public class MultiDimension {
+    public static void main(String[] args) {
+        /*
+            1 2 3
+            4 5 6
+            7 8 9
+         */
+//        int[][] arr = new int[3][]; // adding rows is mandatory but not rows
+        int[][] arr2D = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        }; // this is a direct way
+    }
+}
