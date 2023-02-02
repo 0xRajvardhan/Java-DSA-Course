19aa963fb61b79db27ec33d51b5dc400053e9a2b
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-02-02 22:51:33 +0530

    first and last position problem

---
+++ Binary Search/src/com/Raj/FirstAndLastPosition.java
@@ -0,0 +1,48 @@
+package com.Raj;
+
+public class FirstAndLastPosition {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1, -1};
+
+        //check for first occurence if target first
+        int start = search(nums, target, true);
+        int end = search(nums, target, false);
+
+        ans[0] = start;
+        ans[1] = end;
+
+        return ans;
+    }
+
+    //this function/method just returns the index value of target
+    int search(int[] nums, int target, boolean findStartIndex) {
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            // find middle element
+            // int mid = (start + end)/2; //might be possible that start + end exceeds the int range in java hence may give error
+            int mid = start + (end - start) / 2;
+
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                // potential ans found
+                ans = mid;
+                if (findStartIndex) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
