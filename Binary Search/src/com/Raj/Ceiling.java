e6df40640e25b991b8b04d1ec467daaf66d1191d
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-02-02 20:25:57 +0530

    Ceiling number problem

---
+++ Binary Search/src/com/Raj/Ceiling.java
@@ -0,0 +1,34 @@
+package com.Raj;
+
+public class Ceiling {
+    public static void main(String[] args) {
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int target = 22;
+        int ans = ceiling(arr, target);
+
+        System.out.println(ans);
+    }
+
+    // return the index
+    // return -1 if it does not exist
+    static int ceiling(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            // find middle element
+            // int mid = (start + end)/2; //might be possible that start + end exceeds the int range in java hence may give error
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                //ans found
+                return mid;
+            }
+        }
+        return start;
+    }
+}
