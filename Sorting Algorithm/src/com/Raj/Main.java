932d3569cf82c3a450ad01bd79f1204d5fc54e98
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-02-04 21:37:59 +0530

    Insertion Sorting Algorithm

--- Sorting Algorithm/src/com/Raj/Main.java
+++ Sorting Algorithm/src/com/Raj/Main.java
@@ -7,8 +7,21 @@ public class Main {
         int[] arr = {5, 4, 3, 2, 1};
 //        int[] arr = {1, 2, 3, 4, 5};
 //        bubble(arr);
-        selection(arr);
+//        selection(arr);
+        insertion(arr);
         System.out.println(Arrays.toString(arr));
+    }
+
+    static void insertion(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
     }
 
     static void selection(int[] arr) {
