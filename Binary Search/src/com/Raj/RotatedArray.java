aeff13cc28ccbf09fa1fd412acbfce4ac22c5c91
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-02-04 12:13:23 +0530

    rotated array with duplicate elements problem

--- Binary Search/src/com/Raj/RotatedArray.java
+++ Binary Search/src/com/Raj/RotatedArray.java
@@ -40,6 +40,7 @@ public class RotatedArray {
         return -1;
     }
 
+    // this will not work in duplicate values
     static int findPivot(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
@@ -56,6 +57,43 @@ public class RotatedArray {
                 end = mid - 1;
             } else {
                 start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    static int findPivotWithDuplicates(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            // 4 cases here
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+            // if elements at middle, start and end are equal then just skip the duplicates
+            if (arr[mid] == arr[start] && arr[mid] == arr[end]) {
+                //skip duplicates
+                // NOTE: what if these elements at start and end were the pivots
+                //so check if the start if pivot
+                if (arr[start] > arr[start + 1]) {
+                    return start;
+                }
+                start++;
+                //check whether end is pivot
+                if (arr[end] < arr[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+            }
+            // left side is sorted so pivot should be in right
+            else if (arr[start] < arr[mid] || arr[start] == arr[mid] && arr[mid] > arr[end]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
         return -1;
