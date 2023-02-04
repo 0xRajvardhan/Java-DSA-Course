7ea57af401db91c6addbf761bfb6f6526351acca
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-02-04 21:30:32 +0530

    selection sorting algorithm

--- Sorting Algorithm/src/com/Raj/Main.java
+++ Sorting Algorithm/src/com/Raj/Main.java
@@ -1,13 +1,39 @@
-package Com.Raj;
+package com.Raj;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        // int[] arr = {5, 4, 3, 2, 1};
-        int[] arr = {1, 2, 3, 4, 5};
-        bubble(arr);
+        int[] arr = {5, 4, 3, 2, 1};
+//        int[] arr = {1, 2, 3, 4, 5};
+//        bubble(arr);
+        selection(arr);
         System.out.println(Arrays.toString(arr));
+    }
+
+    static void selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            //find the max item in the remaining array and swap with correct index
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+
+    private static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
     }
 
     static void bubble(int[] arr) {
