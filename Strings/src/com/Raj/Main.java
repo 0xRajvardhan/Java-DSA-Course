9147447787bc6b1686c81bf4c455e8e6f257df5d
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-02-07 12:37:06 +0530

    Learning abour String DataType

---
+++ Strings/src/com/Raj/Main.java
@@ -0,0 +1,15 @@
+package com.Raj;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 419};
+        int num = 10;
+        String name = "Rajvardhan Singh";
+//        System.out.println(name);
+
+        String a = "Rajvardhan";
+        System.out.println(a);
+        a = "Singh";
+        System.out.println(a);
+    }
+}
