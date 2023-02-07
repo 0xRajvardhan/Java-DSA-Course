a5a5d3b5719f289a7a418c027cdf4de19c86866f
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-02-07 13:34:34 +0530

    String Builder in Java

---
+++ Strings/src/com/Raj/SB.java
@@ -0,0 +1,18 @@
+package com.Raj;
+
+public class SB {
+    public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            char ch = (char) ('a' + i);
+            builder.append(ch);
+        }
+        System.out.println(builder.toString());
+
+        builder.deleteCharAt(0);
+        System.out.println(builder);
+
+        builder.reverse();
+        System.out.println(builder);
+    }
+}
