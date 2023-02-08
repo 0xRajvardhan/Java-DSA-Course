3d42f61362fab56b704f65df42daaccc1c80c5fa
Rajvardhan <singhrajvardhan2003@gmail.com>
2023-02-08 17:04:12 +0530

    learning recursion in java

---
+++ Recursion/src/com/Raj/Message.java
@@ -0,0 +1,27 @@
+package com.Raj;
+
+public class Message {
+    public static void main(String[] args) {
+        message();
+    }
+
+    static void message(){
+        System.out.println("Hello world!");
+        message1();
+    }
+    static void message1(){
+        System.out.println("Hello world!");
+        message2();
+    }
+    static void message2(){
+        System.out.println("Hello world!");
+        message3();
+    }
+    static void message3(){
+        System.out.println("Hello world!");
+        message4();
+    }
+    static void message4(){
+        System.out.println("Hello world!");
+    }
+}
